d89fd54e70d60e74af08e1e61d12d0d29b8c6454
huang18679 <365753645@qq.com>
2016-01-10 22:38:33 +0800

    hello

---
+++ 编程题2/编程题2/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  编程题2
+//
+//  Created by LiYaogang on 15/8/25.
+//  Copyright (c) 2015年 XiaoMaGe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ 编程题2/编程题2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  编程题2
+//
+//  Created by LiYaogang on 15/8/25.
+//  Copyright (c) 2015年 XiaoMaGe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
